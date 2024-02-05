f725f0cf02d48ce075d17481ce59da04665394f2
samanthagonzales14 <sgg8xd@umsystem.edu>
2024-02-05 14:22:48 -0600

    Updated Transaction Page

--- src/components/Transactions.js
+++ src/components/Transactions.js
@@ -18,6 +18,13 @@ function TransactionList({transactions}){
   );
 }
 
+function AccountBalance(transaction, currentBalance){
+  if(transaction>=0){
+   return (currentBalance +=transaction ); 
+  }
+  return (currentBalance -= transaction);
+};
+
 export default function Transactions(){
   var testtransactions = [
     {transactionId: 0, accountId: 0, Date: "1/1/24", Description: "Description1", Type: "Purchase1", Amount: 100.00, Balance: 0},
@@ -27,9 +34,51 @@ export default function Transactions(){
     {transactionId: 4, accountId: 0, Date: "1/5/24", Description: "Description5", Type: "Purchase5", Amount: 100.00, Balance: 0}
   ]
 
-  return(
-    
+  /*return(
     <TransactionList transactions={testtransactions} />
-        
-  );
+  );*/
+  return(
+    <>
+    <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Date</th>
+      <th scope="col">Description</th>
+      <th scope="col">Type</th>
+      <th scope="col">State</th>
+      <th scope="col">Amount</th>
+      <th scope="col">Current balance</th>
+    </tr>
+  </thead>
+  <tbody class="table-group-divider">
+    <tr>
+      <td>1/1/2024</td>
+      <td>Purchase from Amazon</td>
+      <td>Debit Card</td>
+      <td>processing</td>     
+      <td>$23.99</td>
+     
+      <td>$2000</td>
+    </tr>
+    <tr>
+      <td>1/30/2024</td>
+      <td>Summerset Apt</td>
+      <td>Debit Card</td>
+      <td>Processed</td>  
+      <td>$800</td> 
+      <td>-$1200</td>
+    </tr>
+    <tr>
+      
+      <td>2/3/2024</td>
+      <td>LLC</td>
+      <td>Deposit</td>
+      <td>processed</td> 
+      <td>$3000</td>
+      <td>$4200</td>
+    </tr>
+  </tbody>
+  </table>
+    </>
+  ); 
 }
