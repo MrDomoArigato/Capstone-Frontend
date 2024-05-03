4c7506e39c6aa96284b64abd25bb3fbced922b70
Andres <90808055+AndresMedin@users.noreply.github.com>
2024-05-02 20:47:19 -0500

    Update Header.test.js

--- src/__test__/Header.test.js
+++ src/__test__/Header.test.js
@@ -1,14 +1,14 @@
-import { render, screen} from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import Header from '../components/Navigation/Header';
 import { Views } from '../enums';
-import '@testing-library/jest-dom/extend-expect'; 
+import '@testing-library/jest-dom/extend-expect';
 
 describe('Header component', () => {
   test('renders correct title for Dashboard view', () => {
     const state = { View: { current: Views.Dashboard } };
     render(<Header state={state} />);
 
-    const headingElement = screen.getByText(/Dashboard/i);
+    const headingElement = screen.getByText("Welcome, friend!");
     expect(headingElement).toBeInTheDocument();
   });
 
@@ -16,20 +16,36 @@ describe('Header component', () => {
     const state = { View: { current: Views.Budget } };
     render(<Header state={state} />);
 
-    const headingElement = screen.getByText(/Budget/i);
+    const headingElement = screen.getByText("Budget");
     expect(headingElement).toBeInTheDocument();
   });
 
-  test('renders correct title for account view', () => {
+  test('renders correct title for Account view', () => {
     const accountName = 'Test Account';
     const state = {
-      View: { current: Views.Account },
-      Account: { current: { accountName: accountName } }
+      View: { current: Views.Account.Transactions }, // Example view from Account
+      Account: { current: { accountName } }
     };
     render(<Header state={state} />);
 
-    const headingElement = screen.getByRole("heading"); //{ name: accountName }; 
+    const headingElement = screen.getByText(accountName);
     expect(headingElement).toBeInTheDocument();
+  });
+
+  test('renders correct title for Profile Settings view', () => {
+    const state = { View: { current: Views.ProfileView } };
+    render(<Header state={state} />);
+
+    const headingElement = screen.getByText("Profile Settings");
+    expect(headingElement).toBeInTheDocument();
+  });
+
+  test('renders an image with correct alt text', () => {
+    const state = { View: { current: Views.Dashboard } };
+    render(<Header state={state} />);
+
+    const imageElement = screen.getByAltText("Capstone");
+    expect(imageElement).toBeInTheDocument();
   });
 });
 
